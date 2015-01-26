37cc867cd26632553b169c81f47078ba343677b6
BizarreKItten <senxx041@morris.umn.edu>
2015-01-26 17:54:18 -0600

    wrote test for GPA calculation functions

--- serverJavascriptTesting.spec.js
+++ serverJavascriptTesting.spec.js
@@ -13,5 +13,13 @@ describe('testing basic functions', function(){
         expect(calculateAverage(20, 20)).toEqual(20);
     });
 
+    it('should return 3', function(){
+        expect(toNumber(B)).toEqual(3);
+    });
+
+    it('should return 3', function(){
+        expect(gpaTotal(B, B, B, 4, 3, 2)).toEqual(3);
+    });
+
 });
 
